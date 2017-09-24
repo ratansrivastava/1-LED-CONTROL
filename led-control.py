04075b538068886c1a3250b898a1d51af08117b6
ratansrivastava <ratan.srivastava885@gmail.com>
2017-09-24 13:24:56 +0530

    Update led-control.py

--- led-control.py
+++ led-control.py
@@ -56,4 +56,4 @@ def action(changePin, action):
    return render_template('main.html', **templateData)
 
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=80, debug=True)
+   app.run(host='127.1.0.0', port=80, debug=True)
